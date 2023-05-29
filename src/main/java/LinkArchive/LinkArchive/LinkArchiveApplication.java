911a9692552bd242166f7b987f7e449af021d867
Hunesu Kim <kewcxea@hanmail.net>
2023-05-29 19:25:59 +0900

    0529 init commit

---
+++ src/main/java/LinkArchive/LinkArchive/LinkArchiveApplication.java
@@ -0,0 +1,13 @@
+package LinkArchive.LinkArchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkArchiveApplication.class, args);
+	}
+
+}
